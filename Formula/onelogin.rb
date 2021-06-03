f963e003ba2d103dff7b5b292081849c1d818ad8
Dominick Caponi <dominick.caponi@onelogin.com>
2021-06-03 11:16:36 -0700

    fixes brew audit errors
    Closes #1.
    
    Signed-off-by: Dominick Caponi <19194626+dcaponi@users.noreply.github.com>

--- Formula/onelogin.rb
+++ Formula/onelogin.rb
@@ -1,9 +1,15 @@
 class Onelogin < Formula
-  desc "A CLI for Using OneLogin"
-  homepage ""
+  desc "CLI for Using OneLogin"
+  homepage "https://github.com/onelogin/onelogin"
   url "https://github.com/onelogin/onelogin/archive/refs/tags/v0.1.15.tar.gz"
   sha256 "5689b232e7dc825764d84789b5789357e3fbbf9df8090edfcca300974e2666ee"
   license "Apache-2.0"
+  head "https://github.com/onelogin/onelogin.git", branch: "develop"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   depends_on "go" => :build
 
