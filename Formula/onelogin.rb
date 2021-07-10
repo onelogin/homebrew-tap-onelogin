cabf12c9efb1ffbc15c7df7c9a4ee1493000138a
Dominick Caponi <dominick.caponi@onelogin.com>
2021-07-10 14:39:31 -0700

    updates onelogin

--- Formula/onelogin.rb
+++ Formula/onelogin.rb
@@ -1,7 +1,7 @@
 class Onelogin < Formula
   desc "CLI for Using OneLogin"
   homepage "https://github.com/onelogin/onelogin"
-  url "https://github.com/onelogin/onelogin/archive/refs/tags/v0.1.15.tar.gz"
+  url "https://github.com/onelogin/onelogin/archive/refs/tags/v0.1.17.tar.gz"
   sha256 "5689b232e7dc825764d84789b5789357e3fbbf9df8090edfcca300974e2666ee"
   license "Apache-2.0"
   head "https://github.com/onelogin/onelogin.git", branch: "develop"
@@ -12,7 +12,7 @@ class Onelogin < Formula
   end
 
   bottle do
-    root_url "https://github.com/onelogin/homebrew-tap-onelogin/releases/download/onelogin-0.1.15"
+    root_url "https://github.com/onelogin/homebrew-tap-onelogin/releases/download/onelogin-0.1.17"
     sha256 cellar: :any_skip_relocation, catalina:     "360e12a4f499ebad76375dff4a62d60746032c4eb9dcd766cbc3820ef89302fa"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "2cf34fea7d244d634416d16e9a3ccf6686ab7501986c83a752aa39c13dd03573"
   end
