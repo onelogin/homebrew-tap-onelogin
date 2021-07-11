580b30ac1ec90c122060c96fe05590583a70f5f8
Dominick Caponi <dominick.caponi@onelogin.com>
2021-07-10 17:31:22 -0700

    onelogin 0.1.17

--- Formula/onelogin.rb
+++ Formula/onelogin.rb
@@ -11,12 +11,6 @@ class Onelogin < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    root_url "https://github.com/onelogin/homebrew-tap-onelogin/releases/download/onelogin-0.1.17"
-    sha256 cellar: :any_skip_relocation, catalina:     "360e12a4f499ebad76375dff4a62d60746032c4eb9dcd766cbc3820ef89302fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2cf34fea7d244d634416d16e9a3ccf6686ab7501986c83a752aa39c13dd03573"
-  end
-
   depends_on "go" => :build
 
   def install
