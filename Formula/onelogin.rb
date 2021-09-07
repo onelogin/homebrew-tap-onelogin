b9e1a77a10287e6dc549e72fe0bba115a7f1683e
Dominick Caponi <d.caponi1@gmail.com>
2021-09-07 15:43:07 -0700

    version bump to v0.1.20

--- Formula/onelogin.rb
+++ Formula/onelogin.rb
@@ -12,7 +12,7 @@ class Onelogin < Formula
   end
 
   bottle do
-    root_url "https://github.com/onelogin/homebrew-tap-onelogin/releases/download/onelogin-0.1.17"
+    root_url "https://github.com/onelogin/homebrew-tap-onelogin/releases/download/onelogin-0.1.20"
     rebuild 1
     sha256 cellar: :any_skip_relocation, catalina:     "0ba79060d7ae89cfc512f64b7062be103f2f3c0179de26d188b25e9cca151906"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "cf0350d31bcc9ef151e8eb2db31d9b08caeabd0fa2dc0c48f2c230730f0e2212"
